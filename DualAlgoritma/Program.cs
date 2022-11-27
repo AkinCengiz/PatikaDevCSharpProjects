09629175d7f1ad7dfa2b97490800127b16e47274
akincengiz <akincengiz@outlook.com.tr>
2022-11-27 21:38:59 +0300

    DualAlgoritma proje dosyası eklendi.

---
+++ DualAlgoritma/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+
+namespace DualAlgoritma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kaç tane ikili üzerinde işlem yapmak istiyorsunuz : ");
+            int dimension = Convert.ToInt32(Console.ReadLine());
+            int[,] dualSerial = new int[dimension, 2];
+            for (int i = 0; i <= dualSerial.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= dualSerial.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine("{0}. sıradaki ikilinin {1}. terimini giriniz : ", i+1, j+1);
+                    dualSerial[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            int[] singleSerial = new int[dimension];
+            for (int i = 0; i <= dualSerial.GetUpperBound(0); i++)
+            {
+                int x = 0;
+                int y = 0;
+                for (int j = 0; j <= dualSerial.GetUpperBound(1); j++)
+                {
+                    if (j == 0)
+                    {
+                        x = dualSerial[i, j];
+                    }
+                    else
+                    {
+                        y = dualSerial[i, j];
+                    }
+                }
+                if (x != y)
+                {
+                    singleSerial[i] = x + y;
+                }
+                else
+                {
+                    singleSerial[i] = x * y * x * y;
+                }
+            }
+
+            Console.WriteLine("===============================================================");
+            
+            foreach (int single in singleSerial)
+            {
+                Console.WriteLine(single);
+            }
+        }
+    }
+}
