5a92fad81f702df699d287fe94dfcc6927accec3
akincengiz <akincengiz@outlook.com.tr>
2022-12-02 22:43:06 +0300

    Consonent proje dosyasi eklendi.

---
+++ Consonent/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Consonent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kontrol edilecek cümleyi giriniz : ");
+            string sentence = Console.ReadLine().ToLower();
+            string[] words = sentence.Split(" ");
+            char[] letters = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+            bool kontrol=false;
+            
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 1; j < words[i].Length; j++)
+                {
+                    kontrol = false;
+                    if (!letters.Contains(words[i][j]))
+                    {
+                        if (!letters.Contains(words[i][j - 1]))
+                        {
+                            kontrol = true;
+                            break;
+                        }
+                    }
+                    
+                    
+                }
+                Console.Write(kontrol == true ? "True\t" : "False\t");
+
+            }
+
+            Console.WriteLine();
+            
+        }
+    }
+}
